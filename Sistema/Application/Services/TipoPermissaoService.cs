2e137d9bbb7f173dcad2274ea3a989aaca2c2e91
luan-smnaccount <luansmn2605@gmail.com>
2025-09-24 14:09:16 -0300

    feat: adicionando Application com Interface e Service do TipoPermissao

---
+++ Sistema/Application/Services/TipoPermissaoService.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sistema.Application.Interfaces;
+using Sistema.Context;
+using Sistema.Models;
+
+namespace Sistema.Application.Services;
+
+public class TipoPermissaoService : ITipoPermissao
+{
+    private readonly AppDbContext _db;
+
+    public TipoPermissaoService(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<TipoPermissao> CreateTipoPermissaoAsync(TipoPermissao entity)
+    {
+        if (entity == null) throw new ArgumentException(nameof(entity));
+        entity.Id = 0;
+
+        await _db.TipoPermissao.AddAsync(entity);
+        try
+        {
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+        catch (Exception ex)
+        {
+            var detail = ex.InnerException?.Message ?? ex.Message;
+            throw new Exception(detail, ex);
+        }
+    }
+}
+
