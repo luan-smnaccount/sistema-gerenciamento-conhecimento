9671d0157e003fbb041ae4dd8026c204fc3b6272
luan-smnaccount <luansmn2605@gmail.com>
2025-10-07 11:12:05 -0300

    fix: correção da modelagem e script Sql com novo padrão de modelagem

--- BancoDados/Script/ScriptSistemaGerenciamentoConhecimento.sql
+++ BancoDados/Script/ScriptSistemaGerenciamentoConhecimento.sql
@@ -3,154 +3,190 @@ GO
 USE db_SistemaGerenciamentoConhecimento
 GO 
 
-CREATE TABLE Cargo(
-    Id SMALLINT NOT NULL IDENTITY PRIMARY KEY,
+CREATE TABLE UsuarioStatus (
+    IdUsuarioStatus TINYINT NOT NULL IDENTITY PRIMARY KEY,
     Nome VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Departamento(
-    Id SMALLINT NOT NULL IDENTITY PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL
+CREATE TABLE Perfil (
+    IdPerfil SMALLINT NOT NULL IDENTITY PRIMARY KEY,
+    Perfil VARCHAR(50) NOT NULL,
+    UsuarioCadastro INT NOT NULL,
+    DataHoraCadastro DATETIME NOT NULL,
+    UsuarioAtualizacao INT NOT NULL,
+    DataHoraAtualizacao DATETIME NOT NULL
 );
 
-CREATE TABLE TipoPermissao(
-    Id TINYINT NOT NULL IDENTITY PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL
+CREATE TABLE Cargo (
+    IdCargo SMALLINT NOT NULL IDENTITY PRIMARY KEY,
+    Nome VARCHAR(50) NOT NULL,
+    UsuarioCadastro INT NOT NULL,
+    DataHoraCadastro DATETIME NOT NULL,
+    UsuarioAtualizacao INT NOT NULL,
+    DataHoraAtualizacao DATETIME NOT NULL
 );
 
-CREATE TABLE StatusUsuario(
-    Id TINYINT NOT NULL IDENTITY PRIMARY KEY,
-    Status VARCHAR(50) NOT NULL
+CREATE TABLE Departamento (
+    IdDepartamento SMALLINT NOT NULL IDENTITY PRIMARY KEY,
+    Nome VARCHAR(50) NOT NULL,
+    UsuarioCadastro INT NOT NULL,
+    DataHoraCadastro DATETIME NOT NULL,
+    UsuarioAtualizacao INT NOT NULL,
+    DataHoraAtualizacao DATETIME NOT NULL
 );
 
-CREATE TABLE Usuario(
-    Id INT NOT NULL IDENTITY PRIMARY KEY,
+CREATE TABLE Usuario (
+    IdUsuario INT NOT NULL IDENTITY PRIMARY KEY,
+    IdUsuarioStatus TINYINT NOT NULL,
+    IdPerfil SMALLINT NOT NULL,
     IdCargo SMALLINT NOT NULL,
     IdDepartamento SMALLINT NOT NULL,
-    IdTipoPermissao TINYINT NOT NULL,
-    IdStatusUsuario TINYINT NOT NULL,
-    Nome VARCHAR(50) NOT NULL,
-    Email VARCHAR(100) NOT NULL,
+    NomeCompleto VARCHAR(120) NOT NULL,
+    Email VARCHAR(320) NOT NULL,
     Senha VARCHAR(60) NOT NULL,
-    DataCriacao DATE NOT NULL,
-    DataAtualizacao DATE NOT NULL,
-
-    FOREIGN KEY (IdCargo) REFERENCES Cargo(Id),
-    FOREIGN KEY (IdDepartamento) REFERENCES Departamento(Id),
-    FOREIGN KEY (IdTipoPermissao) REFERENCES TipoPermissao(Id),
-    FOREIGN KEY (IdStatusUsuario) REFERENCES StatusUsuario(Id)
+    CPF VARCHAR(11) NOT NULL,
+    CEP CHAR(8) NOT NULL,
+    Rua VARCHAR(50) NOT NULL,
+    Cidade VARCHAR(50) NOT NULL,
+    Estado CHAR(2) NOT NULL,
+    NumResidencia VARCHAR(5) NOT NULL,
+    Telefone SMALLINT NOT NULL,
+    UsuarioCadastro INT NOT NULL,
+    DataHoraCadastro DATETIME NOT NULL,
+    UsuarioAtualizacao INT NOT NULL,
+    DataHoraAtualizacao DATETIME NOT NULL,
+    UsuarioAdmissao INT NOT NULL,
+    DataAdmissao DATETIME NOT NULL,
+    FOREIGN KEY (IdUsuarioStatus) REFERENCES UsuarioStatus(IdUsuarioStatus),
+    FOREIGN KEY (IdPerfil) REFERENCES Perfil(IdPerfil),
+    FOREIGN KEY (IdCargo) REFERENCES Cargo(IdCargo),
+    FOREIGN KEY (IdDepartamento) REFERENCES Departamento(IdDepartamento)
 );
 
-CREATE TABLE TipoConteudo(
-    Id TINYINT NOT NULL IDENTITY PRIMARY KEY,
+CREATE TABLE HistoricoVersao (
+    IdHistoricoVersao INT NOT NULL IDENTITY PRIMARY KEY,
+    IdUsuario INT NOT NULL,
+    DescricaoVersao VARCHAR(255) NULL,
+    UsuarioCadastro INT NOT NULL,
+    DataHoraCadastro DATETIME NOT NULL,
+    UsuarioAtualizacao INT NOT NULL,
+    DataHoraAtualizacao DATETIME NOT NULL,
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario)
+);
+
+CREATE TABLE TipoConteudo (
+    IdTipoConteudo TINYINT NOT NULL IDENTITY PRIMARY KEY,
     Nome VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Categoria(
-    Id SMALLINT NOT NULL IDENTITY PRIMARY KEY,
+CREATE TABLE Categoria (
+    IdCategoria SMALLINT NOT NULL IDENTITY PRIMARY KEY,
     Nome VARCHAR(50) NOT NULL,
-    Descricao VARCHAR(100)
+    Descricao VARCHAR(120) NULL
 );
 
-CREATE TABLE Conteudo(
+CREATE TABLE Tag (
+    IdTag SMALLINT NOT NULL IDENTITY PRIMARY KEY,
+    Nome VARCHAR(50) NOT NULL,
+    UsuarioCadastro INT NOT NULL,
+    DataHoraCadastro DATETIME NOT NULL,
+    UsuarioAtualizacao INT NOT NULL,
+    DataHoraAtualizacao DATETIME NOT NULL
+);
+
+CREATE TABLE Conteudo (
     IdConteudo INT NOT NULL IDENTITY PRIMARY KEY,
     IdTipoConteudo TINYINT NOT NULL,
+    IdHistoricoVersao INT NOT NULL,
     IdCategoria SMALLINT NOT NULL,
+    IdTag SMALLINT NOT NULL,
     Conteudo TEXT NOT NULL,
-    Descricao VARCHAR(255),
-    DataCriacao DATE NOT NULL,
-    DataAtualizacao DATE NOT NULL,
-    FOREIGN KEY (IdTipoConteudo) REFERENCES TipoConteudo(Id),
-    FOREIGN KEY (IdCategoria) REFERENCES Categoria(Id)
+    Descricao VARCHAR(120) NULL,
+    LinkVideo VARCHAR(255) NULL,
+    UsuarioCadastro INT NOT NULL,
+    DataHoraCadastro DATETIME NOT NULL,
+    UsuarioAtualizacao INT NOT NULL,
+    DataHoraAtualizacao DATETIME NOT NULL,
+    FOREIGN KEY (IdTipoConteudo) REFERENCES TipoConteudo(IdTipoConteudo),
+    FOREIGN KEY (IdHistoricoVersao) REFERENCES HistoricoVersao(IdHistoricoVersao),
+    FOREIGN KEY (IdCategoria) REFERENCES Categoria(IdCategoria),
+    FOREIGN KEY (IdTag) REFERENCES Tag(IdTag)
 );
 
-CREATE TABLE Tag(
-    Id SMALLINT NOT NULL IDENTITY PRIMARY KEY,
+CREATE TABLE Comentario (
+    IdComentario INT NOT NULL IDENTITY PRIMARY KEY,
+    IdConteudo INT NOT NULL,
+    Comentario VARCHAR(120) NOT NULL,
+    FOREIGN KEY (IdConteudo) REFERENCES Conteudo(IdConteudo)
+);
+
+CREATE TABLE TipoAnexo (
+    IdTipoAnexo TINYINT NOT NULL IDENTITY PRIMARY KEY,
     Nome VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE ConteudoTag(
-    IdConteudo INT NOT NULL,
-    IdTag SMALLINT NOT NULL,
-    PRIMARY KEY (IdConteudo, IdTag),
-    FOREIGN KEY (IdConteudo) REFERENCES Conteudo(IdConteudo),
-    FOREIGN KEY (IdTag) REFERENCES Tag(Id)
-);
-
-CREATE TABLE HistoricoVersao(
-    Id INT NOT NULL IDENTITY PRIMARY KEY,
-    IdConteudo INT NOT NULL,
-    IdUsuario INT NOT NULL,
-    DescricaoVersao VARCHAR(255),
-    DataVersao DATETIME NOT NULL,
-    FOREIGN KEY (IdConteudo) REFERENCES Conteudo(IdConteudo),
-    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id)
-);
-
-CREATE TABLE Comentario(
-    Id INT NOT NULL IDENTITY PRIMARY KEY,
-    IdConteudo INT NOT NULL,
-    IdUsuario INT NOT NULL,
-    Mensagem VARCHAR(100) NOT NULL,
-    FOREIGN KEY (IdConteudo) REFERENCES Conteudo(IdConteudo),
-    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id)
-);
-
-CREATE TABLE TipoAnexo(
-    Id TINYINT NOT NULL IDENTITY PRIMARY KEY,
-    NomeTipoAnexo VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE Anexo(
-    Id INT NOT NULL IDENTITY PRIMARY KEY,
-    IdTipoAnexo TINYINT NOT NULL,
+CREATE TABLE Anexo (
+    IdAnexo INT NOT NULL IDENTITY PRIMARY KEY,
+    IdTipoAnexo INT NOT NULL,
     Nome VARCHAR(50) NOT NULL,
     CaminhoArquivo VARCHAR(300) NOT NULL,
-    FOREIGN KEY (IdTipoAnexo) REFERENCES TipoAnexo(Id)
+    FOREIGN KEY (IdTipoAnexo) REFERENCES TipoAnexo(IdTipoAnexo)
 );
 
-CREATE TABLE ConteudoAnexo(
+CREATE TABLE TipoAvaliacao (
+    IdTipoAvaliacao TINYINT NOT NULL IDENTITY PRIMARY KEY,
+    Nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Avaliacao (
+    IdAvaliacao INT NOT NULL IDENTITY PRIMARY KEY,
+    IdConteudo INT NOT NULL,
+    IdTipoAvaliacao INT NOT NULL,
+    NotaFinal DECIMAL NOT NULL,
+    DataAvaliacao DATE NOT NULL,
+    UsuarioCadastro INT NOT NULL,
+    DataHoraCadastro DATETIME NOT NULL,
+    UsuarioAtualizacao INT NOT NULL,
+    DataHoraAtualizacao DATETIME NOT NULL,
+    FOREIGN KEY (IdConteudo) REFERENCES Conteudo(IdConteudo),
+    FOREIGN KEY (IdTipoAvaliacao) REFERENCES TipoAvaliacao(IdTipoAvaliacao)
+);
+
+CREATE TABLE OpcaoSistema (
+    IdOpcao INT NOT NULL IDENTITY PRIMARY KEY,
+    IdOpcaoSistema INT NULL,
+    DescricaoOpcao VARCHAR(120) NOT NULL,
+    Rota VARCHAR(80) NOT NULL,
+    FOREIGN KEY (IdOpcaoSistema) REFERENCES OpcaoSistema(IdOpcao)
+);
+
+CREATE TABLE Permissao (
+    IdPermissao SMALLINT NOT NULL IDENTITY PRIMARY KEY,
+    Permissao VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE PerfilOpcaoSistemaPermissao (
+    IdPerfil SMALLINT NOT NULL,
+    IdOpcaoSistema INT NOT NULL,
+    IdPermissao INT NOT NULL,
+    PRIMARY KEY (IdPerfil, IdOpcaoSistema, IdPermissao),
+    FOREIGN KEY (IdPerfil) REFERENCES Perfil(IdPerfil),
+    FOREIGN KEY (IdOpcaoSistema) REFERENCES OpcaoSistema(IdOpcao),
+    FOREIGN KEY (IdPermissao) REFERENCES Permissao(IdPermissao)
+);
+
+CREATE TABLE UsuarioComentario (
+    IdUsuario INT NOT NULL,
+    IdComentario INT NOT NULL,
+    PRIMARY KEY (IdUsuario, IdComentario),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
+    FOREIGN KEY (IdComentario) REFERENCES Comentario(IdComentario)
+);
+
+CREATE TABLE ConteudoAnexo (
     IdConteudo INT NOT NULL,
     IdAnexo INT NOT NULL,
     PRIMARY KEY (IdConteudo, IdAnexo),
     FOREIGN KEY (IdConteudo) REFERENCES Conteudo(IdConteudo),
-    FOREIGN KEY (IdAnexo) REFERENCES Anexo(Id)
-);
-
-CREATE TABLE TipoAvaliacao(
-    Id TINYINT NOT NULL IDENTITY PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE Avaliacao(
-    Id INT NOT NULL IDENTITY PRIMARY KEY,
-    IdTipoAvaliacao TINYINT NOT NULL,
-    NotaFinal DECIMAL NOT NULL,
-    DataAvaliacao DATE NOT NULL,
-    DataAtualizacao DATE NOT NULL,
-    FOREIGN KEY (IdTipoAvaliacao) REFERENCES TipoAvaliacao(Id)
-);
-
-CREATE TABLE ConteudoAvaliacao(
-    IdConteudo INT NOT NULL,
-    IdAvaliacao INT NOT NULL,
-    PRIMARY KEY (IdConteudo, IdAvaliacao),
-    FOREIGN KEY (IdConteudo) REFERENCES Conteudo(IdConteudo),
-    FOREIGN KEY (IdAvaliacao) REFERENCES Avaliacao(Id)
-);
-
-CREATE TABLE AnexoAvaliacao(
-    IdAnexo INT NOT NULL,
-    IdAvaliacao INT NOT NULL,
-    PRIMARY KEY (IdAnexo, IdAvaliacao),
-    FOREIGN KEY (IdAnexo) REFERENCES Anexo(Id),
-    FOREIGN KEY (IdAvaliacao) REFERENCES Avaliacao(Id)
-);
-
-CREATE TABLE UsuarioAvaliacao(
-    IdUsuario INT NOT NULL,
-    IdAvaliacao INT NOT NULL,
-    PRIMARY KEY (IdUsuario, IdAvaliacao),
-    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id),
-    FOREIGN KEY (IdAvaliacao) REFERENCES Avaliacao(Id)
+    FOREIGN KEY (IdAnexo) REFERENCES Anexo(IdAnexo)
 );
