0c856efd1eaa64d0f5a05f9644664ad906cbcadf
luan-smnaccount <luansmn2605@gmail.com>
2025-10-20 13:19:02 -0300

    fix: correção geral na estrutura do projeto; adicionada estrutura .sln; adicionado estrutura pastas .Api, .Dominio, .Infra; Adicionadas Interfaces para testes; Adicionado Program.cs dentro do .Api apenas com Interfaces ainda sem Services

---
+++ Sistema/SistemaGestaoConhecimento.Api/Program.cs
@@ -0,0 +1,36 @@
+﻿using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using SistemaGestaoConhecimento.Dominio;
+using SistemaGestaoConhecimento.Infra;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICargo>();
+builder.Services.AddScoped<IDepartamento>();
+builder.Services.AddScoped<IUsuario>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.MapControllers();
+app.Run();
